17ef3bb172f8b232231f368af68e260a9d67f7f4
GFAUnDead <contact@gfaundead.stream>
2023-08-11 14:11:20 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import sqlite3
 # Twitch bot settings
 BOT_USERNAME = "your_bot_username"
 CHANNEL_NAME = "target_channel"
-OAUTH_TOKEN = "your_oauth_token"
+OAUTH_TOKEN = "your_oauth_token"  # Generate from Twitch Developer Dashboard
 
 # Connect to IRC server
 server = "irc.twitch.tv"
@@ -27,5 +27,13 @@ while True:
 
     if data.startswith("PING"):
         irc.send("PONG\n".encode("utf-8"))
+
+    # Check for new follower notifications
+    elif "PRIVMSG" not in data and "NOTICE" in data and f"#{CHANNEL_NAME}" in data:
+        follower_match = re.search(r":(\w+)!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :(.+) has just followed!", data)
+        if follower_match:
+            follower_name = follower_match.group(1)
+            cursor.execute("INSERT INTO followers (follower_name) VALUES (?)", (follower_name,))
+            conn.commit()
 
     time.sleep(1)
