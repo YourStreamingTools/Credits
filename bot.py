fe79af4ea9b4ed6f9f81e1202947f65be12eb4d1
gfaUnDead <contact@gfaundead.stream>
2023-08-13 17:16:54 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,7 +33,6 @@ database_name = f"{CHANNEL_NAME.lower()}.db"
 conn = sqlite3.connect(database_name)
 cursor = conn.cursor()
 
-# Chat interaction loop
 while True:
     data = irc.recv(2048).decode("utf-8")
 
@@ -44,38 +43,62 @@ while True:
 
     # Your API request to get follower information
     follower_api_url = f"https://api.twitch.tv/helix/users/follows?to_id={CHANNEL_ID}"
-    headers = {
-        'Client-ID': '',  # CHANGE TO MAKE THIS WORK
-        'Authorization': 'Bearer {args.bot_token}'
+    follower_headers = {
+        'Client-ID': '', # CHANGE TO MAKE THIS WORK
+        'Authorization': f'Bearer {args.bot_token}'
     }
-    follower_response = requests.get(follower_api_url, headers=headers)
-    cursor.execute("INSERT INTO followers (follower_name, timestamp) VALUES (?, ?)", (follower_name, current_time))
+    follower_response = requests.get(follower_api_url, headers=follower_headers)
+    follower_data = follower_response.json()
+
+    # Extract and insert follower information into the database
+    for follower in follower_data.get('data', []):
+        follower_name = follower['from_name']
+        cursor.execute("INSERT INTO followers (follower_name, timestamp) VALUES (?, ?)", (follower_name, current_time))
 
     # Your API request to get subscriber information
     subscriber_api_url = f"https://api.twitch.tv/helix/subscriptions?broadcaster_id={CHANNEL_ID}"
-    headers = {
-        'Client-ID': '',  # CHANGE TO MAKE THIS WORK
-        'Authorization': 'Bearer {args.bot_token}'
+    subscriber_headers = {
+        'Client-ID': '', # CHANGE TO MAKE THIS WORK
+        'Authorization': f'Bearer {args.bot_token}'
     }
-    subscriber_response = requests.get(subscriber_api_url, headers=headers)
-    cursor.execute("INSERT INTO subscribers (subscriber_name, subscriber_tier, subscription_months, timestamp) VALUES (?, ?, ?, ?)", (subscriber_name, subscriber_tier, subscription_months, current_time))
+    subscriber_response = requests.get(subscriber_api_url, headers=subscriber_headers)
+    subscriber_data = subscriber_response.json()
+
+    # Extract and insert subscriber information into the database
+    for subscriber in subscriber_data.get('data', []):
+        subscriber_name = subscriber['user_name']
+        subscriber_tier = subscriber['tier']
+        subscription_months = subscriber['cumulative_months']
+        cursor.execute("INSERT INTO subscribers (subscriber_name, subscriber_tier, subscription_months, timestamp) VALUES (?, ?, ?, ?)", (subscriber_name, subscriber_tier, subscription_months, current_time))
 
     # Your API request to get cheer information
     cheer_api_url = f"https://api.twitch.tv/helix/bits/leaderboard?user_id={CHANNEL_ID}"
-    headers = {
-        'Client-ID': '',  # CHANGE TO MAKE THIS WORK
-        'Authorization': 'Bearer {args.bot_token}'
+    cheer_headers = {
+        'Client-ID': '', # CHANGE TO MAKE THIS WORK
+        'Authorization': f'Bearer {args.bot_token}'
     }
-    cheer_response = requests.get(cheer_api_url, headers=headers)
-    cursor.execute("INSERT INTO cheers (username, cheer_amount, timestamp) VALUES (?, ?, ?)", (username, cheer_amount, current_time))
+    cheer_response = requests.get(cheer_api_url, headers=cheer_headers)
+    cheer_data = cheer_response.json()
+
+    # Extract and insert cheer information into the database
+    for cheer in cheer_data.get('data', []):
+        username = cheer['user_name']
+        cheer_amount = cheer['score']
+        cursor.execute("INSERT INTO cheers (username, cheer_amount, timestamp) VALUES (?, ?, ?)", (username, cheer_amount, current_time))
 
     # Your API request to get raid information
     raid_api_url = f"https://api.twitch.tv/helix/channels/raids?broadcaster_id={CHANNEL_ID}"
-    headers = {
-        'Client-ID': '',  # CHANGE TO MAKE THIS WORK
-        'Authorization': 'Bearer {args.bot_token}'
+    raid_headers = {
+        'Client-ID': '', # CHANGE TO MAKE THIS WORK
+        'Authorization': f'Bearer {args.bot_token}'
     }
-    raid_response = requests.get(raid_api_url, headers=headers)
-    cursor.execute("INSERT INTO raids (raider_name, viewers, timestamp) VALUES (?, ?, ?)", (raider_name, viewers, current_time))
+    raid_response = requests.get(raid_api_url, headers=raid_headers)
+    raid_data = raid_response.json()
+
+    # Extract and insert raid information into the database
+    for raid in raid_data.get('data', []):
+        raider_name = raid['from_broadcaster_login']
+        viewers = raid['viewers']
+        cursor.execute("INSERT INTO raids (raider_name, viewers, timestamp) VALUES (?, ?, ?)", (raider_name, viewers, current_time))
 
 time.sleep(1)
