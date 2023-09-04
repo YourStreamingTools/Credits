608653f1d9587cf3aa5c97de701a859584871502
GFAUnDead <contact@gfaundead.stream>
2023-09-04 12:47:43 +1000

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['access_token'])) {
 
 // Connect to database
 require_once "db_connect.php";
+include 'database.php';
 
 // Get the current hour in 24-hour format (0-23)
 $currentHour = date('G');
@@ -42,92 +43,10 @@ $stmt->close();
 $database_name = "{$username}.db";
 // Create the SQLite database if it doesn't exist
 if (!file_exists($database_name)) {
-  $conn = new SQLite3($database_name);
-
-  // Create tables for different interactions (followers, subscribers, cheers, raids)
-  $createFollowersTable = "CREATE TABLE IF NOT EXISTS followers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    follower_name TEXT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-  )";
-
-  $createSubscribersTable = "CREATE TABLE IF NOT EXISTS subscribers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    subscriber_name TEXT,
-    subscriber_tier INTEGER,
-    subscription_months INTEGER,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-  )";
-
-  $createCheersTable = "CREATE TABLE IF NOT EXISTS cheers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
-    cheer_amount INTEGER,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-  )";
-
-  $createRaidsTable = "CREATE TABLE IF NOT EXISTS raids (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    raider_name TEXT,
-    viewers INTEGER,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-  )";
-
-  // Execute the table creation queries
-  $conn->exec($createFollowersTable);
-  $conn->exec($createSubscribersTable);
-  $conn->exec($createCheersTable);
-  $conn->exec($createRaidsTable);
-
-  $conn->close();
+    createTables($database_name);
 }
-
-$database_name = "{$username}.db";
 if (isset($_POST['resetDatabase'])) {
-  // Delete the SQLite database file
-  if (file_exists($database_name)) {
-    unlink($database_name);
-  }
-
-  // Recreate the SQLite database and tables
-  $conn = new SQLite3($database_name);
-
-  // Create tables for different interactions (followers, subscribers, cheers, raids)
-  $createFollowersTable = "CREATE TABLE IF NOT EXISTS followers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    follower_name TEXT,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-  )";
-
-  $createSubscribersTable = "CREATE TABLE IF NOT EXISTS subscribers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    subscriber_name TEXT,
-    subscriber_tier INTEGER,
-    subscription_months INTEGER,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-  )";
-
-  $createCheersTable = "CREATE TABLE IF NOT EXISTS cheers (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT,
-    cheer_amount INTEGER,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-  )";
-
-  $createRaidsTable = "CREATE TABLE IF NOT EXISTS raids (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    raider_name TEXT,
-    viewers INTEGER,
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-  )";
-
-  // Execute the table creation queries
-  $conn->exec($createFollowersTable);
-  $conn->exec($createSubscribersTable);
-  $conn->exec($createCheersTable);
-  $conn->exec($createRaidsTable);
-
-  $conn->close();
+    resetDatabase($database_name);
 }
 
 $totalRaidersToday = 0;
