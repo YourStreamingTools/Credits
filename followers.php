6f8de9e142a3e7575ca85a81f1c60d2c1d2bc644
GFAUnDead <contact@gfaundead.stream>
2023-09-11 11:37:36 +1000

    Create followers.php

---
+++ followers.php
@@ -0,0 +1,138 @@
+<?php
+// Initialize the session
+session_start();
+
+// check if user is logged in
+if (!isset($_SESSION['access_token'])) {
+    header('Location: login.php');
+    exit();
+}
+
+// Connect to database
+require_once "db_connect.php";
+include 'database.php';
+
+// Get the current hour in 24-hour format (0-23)
+$currentHour = date('G');
+// Initialize the greeting variable
+$greeting = '';
+// Check if it's before 12 PM (noon)
+if ($currentHour < 12) {
+    $greeting = "Good morning";
+} else {
+    $greeting = "Good afternoon";
+}
+
+// Fetch the user's data from the database based on the access_token
+$access_token = $_SESSION['access_token'];
+
+$stmt = $conn->prepare("SELECT * FROM users WHERE access_token = ?");
+$stmt->bind_param("s", $access_token);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+$user_id = $user['id'];
+$username = $user['username'];
+$broadcasterID = $user['twitch_user_id'];
+$twitchDisplayName = $user['twitch_display_name'];
+$twitch_profile_image_url = $user['profile_image'];
+$is_admin = ($user['is_admin'] == 1);
+$accessToken = $access_token;
+
+// API endpoint to fetch followers
+$followersURL = "https://api.twitch.tv/helix/users/follows?to_id=$broadcasterID";
+$clientID = ''; // CHANGE TO MAKE THIS WORK
+
+// Set up cURL request with headers
+$curl = curl_init($followersURL);
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'Authorization: Bearer ' . $accessToken,
+    'Client-ID: ' . $clientID
+]);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+// Execute cURL request
+$response = curl_exec($curl);
+
+if ($response === false) {
+    // Handle cURL error
+    echo 'cURL error: ' . curl_error($curl);
+    exit;
+}
+
+$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+if ($httpCode !== 200) {
+    // Handle non-successful HTTP response
+    $HTTPError = 'HTTP error: ' . $httpCode;
+    exit;
+}
+
+curl_close($curl);
+
+// Process and display follower information
+$followersData = json_decode($response, true);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>YourStreamingTools - Twitch Mods</title>
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
+    <script src="https://cdn.yourstreaming.tools/js/about.js"></script>
+  	<link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.jpeg">
+  	<link rel="apple-touch-icon" href="https://cdn.yourstreaming.tools/img/logo.jpeg">
+    <script src="sendtodiscord.js"></script>
+    <script>var webhookURL = "<?php echo $webhookURL; ?>";</script>
+  </head>
+<body>
+<!-- Navigation -->
+<div class="title-bar" data-responsive-toggle="mobile-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+<nav class="top-bar stacked-for-medium" id="mobile-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+      <li class="menu-text">YourStreamingTools</li>
+      <li class="is-active"><a href="index.php">Dashboard</a></li>
+      <li><a href="bot.php">Bot</a></li>
+      <li>
+        <a>Profile</a>
+        <ul class="vertical menu" data-dropdown-menu>
+          <li><a href="profile.php">View Profile</a></li>
+          <li><a href="mods.php">View Mods</a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourStreamingTools. All rights reserved.</a></li>
+    </ul>
+  </div>
+</nav>
+<!-- /Navigation -->
+
+<div class="row column">
+<br>
+<h1><?php echo "$greeting, <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>$twitchDisplayName!"; ?></h1>
+<br>
+<?php if ($httpCode !== 200) { echo $HTTPError; exit; } else { ?>
+    <h1>Your Followers:</h1>
+    <ul>
+        <?php foreach ($followersData['data'] as $follower) : 
+            $followerDisplayName = $follower['from_name'];
+            echo "<li>$followerDisplayName</li>";
+        ?><?php endforeach; ?>
+    </ul>
+<?php } ?>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>$(document).foundation();</script>
+</body>
+</html>
