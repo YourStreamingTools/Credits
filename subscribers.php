1026dd1846944f223411410de3c0027d51e5ce7f
GFAUnDead <contact@gfaundead.stream>
2023-09-11 14:11:46 +1000

    Create subscribers.php

---
+++ subscribers.php
@@ -0,0 +1,213 @@
+<?php error_reporting(E_ALL); ini_set('display_errors', 1); ?>
+<?php
+// Initialize the session
+session_start();
+
+// check if user is logged in
+if (!isset($_SESSION['access_token'])) {
+    header('Location: login.php');
+    exit();
+}
+
+// Connect to database
+require_once "db_connect.php";
+include 'database.php';
+
+// Get the current hour in 24-hour format (0-23)
+$currentHour = date('G');
+// Initialize the greeting variable
+$greeting = '';
+// Check if it's before 12 PM (noon)
+if ($currentHour < 12) {
+    $greeting = "Good morning";
+} else {
+    $greeting = "Good afternoon";
+}
+
+// Fetch the user's data from the database based on the access_token
+$access_token = $_SESSION['access_token'];
+
+$stmt = $conn->prepare("SELECT * FROM users WHERE access_token = ?");
+$stmt->bind_param("s", $access_token);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+$user_id = $user['id'];
+$username = $user['username'];
+$broadcasterID = $user['twitch_user_id'];
+$twitchDisplayName = $user['twitch_display_name'];
+$twitch_profile_image_url = $user['profile_image'];
+$is_admin = ($user['is_admin'] == 1);
+$accessToken = $access_token;
+
+// API endpoint to fetch subscribers
+$subscribersURL = "https://api.twitch.tv/helix/subscriptions?broadcaster_id=$broadcasterID";
+$clientID = ''; // CHANGE TO MAKE THIS WORK
+
+$allSubscribers = [];
+do {
+    // Set up cURL request with headers
+    $curl = curl_init($subscribersURL);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, [
+        'Authorization: Bearer ' . $accessToken,
+        'Client-ID: ' . $clientID
+    ]);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute cURL request
+    $response = curl_exec($curl);
+
+    if ($response === false) {
+        // Handle cURL error
+        echo 'cURL error: ' . curl_error($curl);
+        exit;
+    }
+
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if ($httpCode !== 200) {
+        // Handle non-successful HTTP response
+        $HTTPError = 'HTTP error: ' . $httpCode;
+        exit;
+    }
+
+    curl_close($curl);
+
+    // Process and append subscriber information to the array
+    $subscribersData = json_decode($response, true);
+    $allSubscribers = array_merge($allSubscribers, $subscribersData['data']);
+
+    // Check if there are more pages of subscribers
+    $cursor = $subscribersData['pagination']['cursor'] ?? null;
+    $subscribersURL = "https://api.twitch.tv/helix/subscriptions?broadcaster_id=$broadcasterID&after=$cursor";
+
+} while ($cursor);
+
+// Process and display subscriber information
+$subscribersData = json_decode($response, true);
+$allSubscribers = $subscribersData['data'];
+
+// Function to get subscription tier based on cumulative months
+function getSubscriptionTier($cumulativeMonths) {
+    if ($cumulativeMonths >= 1000) {
+        return 'Tier 1';
+    } elseif ($cumulativeMonths >= 2000) {
+        return 'Tier 2';
+    } elseif ($cumulativeMonths >= 3000) {
+        return 'Tier 3';
+    } else {
+        return 'Unknown';
+    }
+}
+
+// Retrieve the subscription tier for each subscriber
+foreach ($allSubscribers as &$subscriber) {
+    $cumulativeMonths = $subscriber['cumulative_months'] ?? 0;
+    $subscriber['subscription_tier'] = getSubscriptionTier($cumulativeMonths);
+}
+
+// Number of subscribers per page
+$subscribersPerPage = 50;
+
+// Calculate the total number of pages
+$totalPages = ceil(count($allSubscribers) / $subscribersPerPage);
+
+// Current page (default to 1 if not specified)
+$currentPage = isset($_GET['page']) ? max(1, min($totalPages, intval($_GET['page']))) : 1;
+
+// Calculate the start and end index for the current page
+$startIndex = ($currentPage - 1) * $subscribersPerPage;
+$endIndex = $startIndex + $subscribersPerPage;
+
+// Get subscribers for the current page
+$subscribersForCurrentPage = array_slice($allSubscribers, $startIndex, $subscribersPerPage);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>YourStreamingTools - Twitch Subscribers</title>
+    <link rel="stylesheet" href="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+    <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
+    <link rel="stylesheet" href="pagination.css">
+    <script src="https://cdn.yourstreaming.tools/js/about.js"></script>
+  	<link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.jpeg">
+  	<link rel="apple-touch-icon" href="https://cdn.yourstreaming.tools/img/logo.jpeg">
+    <script src="sendtodiscord.js"></script>
+    <script>var webhookURL = "<?php echo $webhookURL; ?>";</script>
+  </head>
+<body>
+<!-- Navigation -->
+<div class="title-bar" data-responsive-toggle="mobile-menu" data-hide-for="medium">
+  <button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+  <div class="title-bar-title">Menu</div>
+</div>
+<nav class="top-bar stacked-for-medium" id="mobile-menu">
+  <div class="top-bar-left">
+    <ul class="dropdown vertical medium-horizontal menu" data-responsive-menu="drilldown medium-dropdown hinge-in-from-top hinge-out-from-top">
+      <li class="menu-text">YourStreamingTools</li>
+      <li><a href="index.php">Dashboard</a></li>
+      <li><a href="bot.php">Bot</a></li>
+      <li>
+        <a>Profile</a>
+        <ul class="vertical menu" data-dropdown-menu>
+          <li><a href="profile.php">View Profile</a></li>
+          <li><a>Twitch Data</a>
+          <ul class="vertical menu" data-dropdown-menu>
+            <li><a href="mods.php">View Mods</a></li>
+            <li><a href="followers.php">View Followers</a></li>
+            <li><a href="subscribers.php">View Subscribers</a></li>
+            <li><a href="vips.php">View VIPs</a></li>
+          </ul>
+          </li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><a class="popup-link" onclick="showPopup()">&copy; 2023 YourStreamingTools. All rights reserved.</a></li>
+    </ul>
+  </div>
+</nav>
+<!-- /Navigation -->
+
+<div class="row column">
+  <br>
+  <h1><?php echo "$greeting, <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>$twitchDisplayName!"; ?></h1>
+  <br>
+  <h1>Your Subscribers:</h1>
+  <div class="subscribers-grid">
+      <?php foreach ($subscribersForCurrentPage as $subscriber) : 
+          $subscriberDisplayName = $subscriber['user_name'];
+          $isGift = $subscriber['is_gift'] ?? false;
+          $gifterName = $subscriber['gifter_name'] ?? '';
+          if ($isGift) {
+              echo "<div class='subscriber'><span>$subscriberDisplayName</span><span>Gift Sub from $gifterName</span></div>";
+          } else {
+              echo "<div class='subscriber'><span>$subscriberDisplayName</span></div>";
+          }
+      ?>
+      <?php endforeach; ?>
+  </div>
+
+  <!-- Pagination -->
+  <div class="pagination">
+      <?php if ($totalPages > 1) : ?>
+          <?php for ($page = 1; $page <= $totalPages; $page++) : ?>
+              <?php if ($page === $currentPage) : ?>
+                  <span class="current-page"><?php echo $page; ?></span>
+              <?php else : ?>
+                  <a href="?page=<?php echo $page; ?>"><?php echo $page; ?></a>
+              <?php endif; ?>
+          <?php endfor; ?>
+      <?php endif; ?>
+  </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>$(document).foundation();</script>
+</body>
+</html>
