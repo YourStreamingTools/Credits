f7bc667faf1080c51ce88d485a48756b004b39be
gfaUnDead <contact@gfaundead.stream>
2023-09-11 22:31:24 +1000

    Update vips.php

--- vips.php
+++ vips.php
@@ -43,34 +43,59 @@ $accessToken = $access_token;
 $vipsURL = "https://api.twitch.tv/helix/channels/vips?broadcaster_id=$broadcasterID";
 $clientID = ''; // CHANGE TO MAKE THIS WORK
 
-// Set up cURL request with headers
-$curl = curl_init($vipsURL);
-curl_setopt($curl, CURLOPT_HTTPHEADER, [
-    'Authorization: Bearer ' . $accessToken,
-    'Client-ID: ' . $clientID
-]);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$allVIPs = [];
+do {
+    // Set up cURL request with headers
+    $curl = curl_init($vipsURL);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, [
+        'Authorization: Bearer ' . $accessToken,
+        'Client-ID: ' . $clientID
+    ]);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-// Execute cURL request
-$response = curl_exec($curl);
+    // Execute cURL request
+    $response = curl_exec($curl);
 
-if ($response === false) {
-    // Handle cURL error
-    echo 'cURL error: ' . curl_error($curl);
-    exit;
-}
+    if ($response === false) {
+        // Handle cURL error
+        echo 'cURL error: ' . curl_error($curl);
+        exit;
+    }
 
-$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-if ($httpCode !== 200) {
-    // Handle non-successful HTTP response
-    $HTTPError = 'HTTP error: ' . $httpCode;
-    exit;
-}
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if ($httpCode !== 200) {
+        // Handle non-successful HTTP response
+        $HTTPError = 'HTTP error: ' . $httpCode;
+        exit;
+    }
 
-curl_close($curl);
+    curl_close($curl);
 
-// Process and display VIP information
-$vipsData = json_decode($response, true);
+    // Process and append VIP information to the array
+    $vipsData = json_decode($response, true);
+    $allVIPs = array_merge($allVIPs, $vipsData['data']);
+
+    // Check if there are more pages of VIPs
+    $cursor = $vipsData['pagination']['cursor'] ?? null;
+    $vipsURL = "https://api.twitch.tv/helix/channels/vips?broadcaster_id=$broadcasterID&after=$cursor";
+
+} while ($cursor);
+
+// Number of VIPs per page
+$vipsPerPage = 50;
+
+// Calculate the total number of pages
+$totalPages = ceil(count($allVIPs) / $vipsPerPage);
+
+// Current page (default to 1 if not specified)
+$currentPage = isset($_GET['page']) ? max(1, min($totalPages, intval($_GET['page']))) : 1;
+
+// Calculate the start and end index for the current page
+$startIndex = ($currentPage - 1) * $vipsPerPage;
+$endIndex = $startIndex + $vipsPerPage;
+
+// Get VIPs for the current page
+$VIPsForCurrentPage = array_slice($allVIPs, $startIndex, $vipsPerPage);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -136,6 +161,19 @@ $vipsData = json_decode($response, true);
           <span><?php echo $vipDisplayName; ?></span>
       </div>
       <?php endforeach; ?>
+  </div>
+
+  <!-- Pagination -->
+  <div class="pagination">
+      <?php if ($totalPages > 1) : ?>
+          <?php for ($page = 1; $page <= $totalPages; $page++) : ?>
+              <?php if ($page === $currentPage) : ?>
+                  <span class="current-page"><?php echo $page; ?></span>
+              <?php else : ?>
+                  <a href="?page=<?php echo $page; ?>"><?php echo $page; ?></a>
+              <?php endif; ?>
+          <?php endfor; ?>
+      <?php endif; ?>
   </div>
 </div>
 
