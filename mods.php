c4b5726204ec17bba4068a5e92fc63103aad3a60
GFAUnDead <contact@gfaundead.stream>
2023-09-11 11:46:50 +1000

    Update mods.php

--- mods.php
+++ mods.php
@@ -103,6 +103,7 @@ $moderatorsData = json_decode($response, true);
         <ul class="vertical menu" data-dropdown-menu>
           <li><a href="profile.php">View Profile</a></li>
           <li><a href="mods.php">View Mods</a></li>
+          <li><a href="followers.php">View Followers</a></li>
           <li><a href="logout.php">Logout</a></li>
         </ul>
       </li>
