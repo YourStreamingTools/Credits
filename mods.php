3f8caa4c86581ab9ac7204289a0b3f6f235bb59b
GFAUnDead <contact@gfaundead.stream>
2023-09-11 11:07:36 +1000

    Update mods.php

--- mods.php
+++ mods.php
@@ -132,7 +132,6 @@ $moderatorsData = json_decode($response, true);
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
 <script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
-<script src="https://cdn.yourlist.online/js/darkmode.js"></script>
 <script>$(document).foundation();</script>
 </body>
 </html>
