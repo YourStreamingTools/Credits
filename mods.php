631172b1bdf0da19fee57d6047b03b662e207cc9
gfaUnDead <contact@gfaundead.stream>
2023-09-11 21:57:37 +1000

    Update mods.php

--- mods.php
+++ mods.php
@@ -43,34 +43,60 @@ $accessToken = $access_token;
 $moderatorsURL = "https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=$broadcasterID";
 $clientID = ''; // CHANGE TO MAKE THIS WORK
 
-// Set up cURL request with headers
-$curl = curl_init($moderatorsURL);
-curl_setopt($curl, CURLOPT_HTTPHEADER, [
-    'Authorization: Bearer ' . $accessToken,
-    'Client-ID: ' . $clientID
-]);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$allModerators = [];
+do {
+    // Set up cURL request with headers
+    $curl = curl_init($moderatorsURL);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, [
+        'Authorization: Bearer ' . $accessToken,
+        'Client-ID: ' . $clientID
+    ]);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-// Execute cURL request
-$response = curl_exec($curl);
+    // Execute cURL request
+    $response = curl_exec($curl);
 
-if ($response === false) {
-    // Handle cURL error
-    echo 'cURL error: ' . curl_error($curl);
-    exit;
-}
+    if ($response === false) {
+        // Handle cURL error
+        echo 'cURL error: ' . curl_error($curl);
+        exit;
+    }
 
-$httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-if ($httpCode !== 200) {
-    // Handle non-successful HTTP response
-    $HTTPError = 'HTTP error: ' . $httpCode;
-    exit;
-}
+    $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    if ($httpCode !== 200) {
+        // Handle non-successful HTTP response
+        $HTTPError = 'HTTP error: ' . $httpCode;
+        exit;
+    }
 
-curl_close($curl);
+    curl_close($curl);
 
-// Process and display moderator information
-$moderatorsData = json_decode($response, true);
+    // Process and append moderator information to the array
+    $moderatorsData = json_decode($response, true);
+    $allModerators = array_merge($allModerators, $moderatorsData['data']);
+
+    // Check if there are more pages of moderators
+    $cursor = $moderatorsData['pagination']['cursor'] ?? null;
+    $moderatorsURL = "https://api.twitch.tv/helix/moderation/moderators?broadcaster_id=$broadcasterID&after=$cursor";
+
+} while ($cursor);
+
+// Number of moderators per page
+$moderatorsPerPage = 50;
+
+// Calculate the total number of pages
+$totalPages = ceil(count($allModerators) / $moderatorsPerPage);
+
+// Current page (default to 1 if not specified)
+$currentPage = isset($_GET['page']) ? max(1, min($totalPages, intval($_GET['page']))) : 1;
+
+// Calculate the start and end index for the current page
+$startIndex = ($currentPage - 1) * $moderatorsPerPage;
+$endIndex = $startIndex + $moderatorsPerPage;
+
+// Get moderators for the current page
+$moderatorsForCurrentPage = array_slice($allModerators, $startIndex, $moderatorsPerPage);
+?>
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -127,15 +153,29 @@ $moderatorsData = json_decode($response, true);
 <br>
 <h1><?php echo "$greeting, <img id='profile-image' src='$twitch_profile_image_url' width='50px' height='50px' alt='$twitchDisplayName Profile Image'>$twitchDisplayName!"; ?></h1>
 <br>
-<?php if ($httpCode !== 200) { echo $HTTPError; exit; } else { ?>
-    <h1>Your Moderators:</h1>
-    <ul>
-        <?php foreach ($moderatorsData['data'] as $moderator) : 
-            $modDisplayName = $moderator['user_name'];
-            echo "<li>$modDisplayName</li>";
-        ?><?php endforeach; ?>
-    </ul>
-<?php } ?>
+<h1>Your Moderators:</h1>
+  <div class="moderators-grid">
+      <?php foreach ($moderatorsData['data'] as $moderator) : 
+          $modDisplayName = $moderator['user_name'];
+      ?>
+      <div class="moderator">
+          <span><?php echo $modDisplayName; ?></span>
+      </div>
+      <?php endforeach; ?>
+  </div>
+
+  <!-- Pagination -->
+  <div class="pagination">
+      <?php if ($totalPages > 1) : ?>
+          <?php for ($page = 1; $page <= $totalPages; $page++) : ?>
+              <?php if ($page === $currentPage) : ?>
+                  <span class="current-page"><?php echo $page; ?></span>
+              <?php else : ?>
+                  <a href="?page=<?php echo $page; ?>"><?php echo $page; ?></a>
+              <?php endif; ?>
+          <?php endfor; ?>
+      <?php endif; ?>
+  </div>
 </div>
 
 <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
