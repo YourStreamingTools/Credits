a3ce196f7e1fa80f8053d7dba9bfa956980204d7
SimDifRandomHD <contact@gfaundead.stream>
2023-10-06 16:02:17 -0500

    Update subscribers.php

--- subscribers.php
+++ subscribers.php
@@ -112,8 +112,8 @@ $displaySearchBar = count($allSubscribers) > $subscribersPerPage;
     <link rel="stylesheet" href="https://cdn.yourstreaming.tools/css/custom.css">
     <link rel="stylesheet" href="pagination.css">
     <script src="https://cdn.yourstreaming.tools/js/about.js"></script>
-  	<link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.jpeg">
-  	<link rel="apple-touch-icon" href="https://cdn.yourstreaming.tools/img/logo.jpeg">
+    <link rel="icon" href="https://cdn.yourstreaming.tools/img/logo.jpeg">
+    <link rel="apple-touch-icon" href="https://cdn.yourstreaming.tools/img/logo.jpeg">
     <script src="sendtodiscord.js"></script>
     <script>var webhookURL = "<?php echo $webhookURL; ?>";</script>
   </head>
