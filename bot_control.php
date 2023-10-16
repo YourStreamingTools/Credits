a703afd7c404dd96fdfd776aa03d47095eec0a95
gfaUnDead <contact@gfaundead.stream>
2023-10-16 20:16:53 +1100

    Update bot_control.php

--- bot_control.php
+++ bot_control.php
@@ -1,74 +1,65 @@
 <?php
+$botScriptPath = "/var/www/bot/bot.py";
+$statusScriptPath = "/var/www/bot/status.py";
+
 if (isset($_POST['runBot'])) {
-// Check if the bot is already running
-if (isBotRunning($username)) {
-  $statusOutput = shell_exec("python status.py -channel $username");
-  $pid = intval(preg_replace('/\D/', '', $statusOutput));
-  $statusOutput = "Bot is already running. Process ID: $pid";
-} else {
-  // Execute the Python script with the channel name as an argument
-  $output = shell_exec("python bot.py -channel $username -channelid $twitchUserId -token $authToken > /dev/null 2>&1 &");
-
-  // Sleep for a few seconds to allow the process to start
-  sleep(3);
-
-  // Fetch the bot's PID from status.py
-  $statusOutput = shell_exec("python status.py -channel $username");
-  $pid = intval(trim($statusOutput));
-  }
+    if (isBotRunning($statusScriptPath, $username)) {
+        $statusOutput = shell_exec("python $statusScriptPath -channel $username");
+        $pid = intval(preg_replace('/\D/', '', $statusOutput));
+        $statusOutput = "Bot is already running. Process ID: $pid";
+    } else {
+        startBot($botScriptPath, $username, $twitchUserId, $authToken);
+        $statusOutput = "Bot started successfully.";
+    }
 }
 
 if (isset($_POST['botStatus'])) {
-  $statusOutput = shell_exec("python status.py -channel $username");
-  $pid = intval(preg_replace('/\D/', '', $statusOutput));
+    $statusOutput = getBotStatus($statusScriptPath, $username);
 }
 
 if (isset($_POST['killBot'])) {
-  $statusOutput = shell_exec("python status.py -channel $username");
-  $pid = intval(preg_replace('/\D/', '', $statusOutput));
-
-  // Kill the bot's process
-  $killprocess = shell_exec("kill $pid > /dev/null 2>&1 &");
-    
-  // Sleep for a few seconds to allow the process to start
-  sleep(3);
-
-  // Remove the bot's PID from the session
-  $pid = '';
-    
-  $statusOutput = "Bot Status: Stopped.";
+    $pid = getBotPID($statusScriptPath, $username);
+    if ($pid > 0) {
+        killBot($pid);
+        $statusOutput = "Bot stopped successfully.";
+    } else {
+        $statusOutput = "Bot is not running.";
+    }
 }
 
 if (isset($_POST['restartBot'])) {
-  // Check if the bot is running and stop it
-  if (isBotRunning($username)) {
-    $statusOutput = shell_exec("python status.py -channel $username");
-    $pid = intval(preg_replace('/\D/', '', $statusOutput));
-
-    // Kill the bot's process
-    $killprocess = shell_exec("kill $pid > /dev/null 2>&1 &");
-    
-    // Sleep for a few seconds to allow the process to start
-    sleep(3);
-
-    // Start the bot
-    $output = shell_exec("python bot.py -channel $username -channelid $twitchUserId -token $authToken > /dev/null 2>&1 &");
-  
-    // Sleep for a few seconds to allow the process to start
-    sleep(3);
-
-    // Fetch the bot's PID from status.py
-    $statusOutput = shell_exec("python status.py -channel $username");
-    $pid = intval(preg_replace('/\D/', '', $statusOutput));
-    $statusOutput = "Bot restarted successfully. Process ID: $pid";
-  } else {
-    $statusOutput = "Can't restart bot, bot is not running.";
-  }
+    $pid = getBotPID($statusScriptPath, $username);
+    if ($pid > 0) {
+        killBot($pid);
+        startBot($botScriptPath, $username, $twitchUserId, $authToken);
+        $statusOutput = "Bot restarted successfully.";
+    } else {
+        $statusOutput = "Bot is not running.";
+    }
 }
 
-function isBotRunning($username) {
-  $statusOutput = shell_exec("python status.py -channel $username");
-  $pid = intval(preg_replace('/\D/', '', $statusOutput));
-  return ($pid > 0);
+function isBotRunning($statusScriptPath, $username) {
+    $pid = getBotPID($statusScriptPath, $username);
+    return ($pid > 0);
+}
+
+function getBotPID($statusScriptPath, $username) {
+    $statusOutput = shell_exec("python $statusScriptPath -channel $username");
+    return intval(preg_replace('/\D/', '', $statusOutput));
+}
+
+function getBotStatus($statusScriptPath, $username) {
+    return shell_exec("python $statusScriptPath -channel $username");
+}
+
+function startBot($botScriptPath, $username, $twitchUserId, $authToken) {
+    $command = "python $botScriptPath -channel $username -channelid $twitchUserId -token $authToken > /dev/null 2>&1 &";
+    shell_exec($command);
+    sleep(3);
+}
+
+function killBot($pid) {
+    shell_exec("kill $pid > /dev/null 2>&1 &");
+    sleep(3);
 }
 ?>
